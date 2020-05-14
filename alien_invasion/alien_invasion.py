0259e079ee6feea1068645174400373a4000a839
Sudo-Sam2017 <31296306+Sudo-Sam2017@users.noreply.github.com>
2020-05-14 10:46:10 -0400

    05_14_2020 -- Added left/right movement to player ship

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	#Initialize pygame, settings, and screen object.
@@ -13,21 +14,18 @@ def run_game():
 	pygame.display.set_caption("Alien Invastion")
 	
 	#Make a Ship.
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
 	
 	#Start the main loop for the game.
 	while True:
+		gf.check_events(ship)
+		ship.update()
+		gf.update_screen(ai_settings, screen, ship)
 		
-		#Watch for keyboard and mouse events.
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+
 			
-		#Redraw the screen during each pass through the loop.
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
+
 		
-		#Make the most recently drawn screen visible
-		pygame.display.flip()
+
 		
 run_game()
