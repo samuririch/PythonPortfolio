12e483c536e81b409695da0d16e72411e0e4c353
Sudo-Sam2017 <31296306+Sudo-Sam2017@users.noreply.github.com>
2020-05-18 12:02:53 -0400

    05_18_20 - Added bullet firing functionality to player ship.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,10 +1,11 @@
 import sys
 
 import pygame
-
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+
 
 def run_game():
 	#Initialize pygame, settings, and screen object.
@@ -12,20 +13,26 @@ def run_game():
 	ai_settings = Settings()
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invastion")
-	
-	#Make a Ship.
+   #Make a Ship.
 	ship = Ship(ai_settings, screen)
+  #Make a group to store bullets in.
+	bullets = Group()
 	
 	#Start the main loop for the game.
 	while True:
-		gf.check_events(ship)
+		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_screen(ai_settings, screen, ship)
+		gf.update_bullets(bullets)
+		#bullets.update()
+		gf.update_screen(ai_settings, screen, ship, bullets)
+		
+		#Get rid of bullets that have disappeared
+		for bullet in bullets.copy():
+			if bullet.rect.bottom <= 0:
+				bullets.remove(bullet)
+		print(len(bullets))
+		
+		
 		
 
-			
-
-		
-
-		
 run_game()
